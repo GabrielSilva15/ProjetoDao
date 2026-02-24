364e00ffe054b846479a985befe5b4b3b4095969
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-02-24 19:28:43 -0300

    Department entity class

---
+++ src/main/java/org/application/Program.java
@@ -0,0 +1,14 @@
+package org.application;
+
+import org.model.entities.Department;
+
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department dp = new Department("IT", 1);
+        System.out.println(dp);
+
+        
+    }
+}
