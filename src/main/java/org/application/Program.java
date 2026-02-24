0514482b7935e98f264b6186a58ebefc2755a8ae
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-02-24 19:34:47 -0300

    Seller entity class

--- src/main/java/org/application/Program.java
+++ src/main/java/org/application/Program.java
@@ -1,14 +1,18 @@
 package org.application;
 
 import org.model.entities.Department;
+import org.model.entities.Seller;
+
+import java.util.Date;
 
 
 public class Program {
     public static void main(String[] args) {
 
         Department dp = new Department("IT", 1);
-        System.out.println(dp);
+        Seller sl = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,dp);
+        System.out.println(sl);
 
-        
+
     }
 }
